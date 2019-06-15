12268b645a9f39e10bcc248a7cf0b72e99362fa4
Chris Wolfe <me@crlfe.ca>
2019-06-15 14:40:24 -0400

    Enable jsdoc checking in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:jsdoc/recommended",
+    "plugin:prettier/recommended"
+  ],
   env: {
     es6: true
   },
@@ -7,9 +11,22 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
+  plugins: ["jsdoc", "prettier"],
   rules: {
     eqeqeq: "error",
     "no-floating-decimal": "error",
-    "no-throw-literal": "error"
+    "no-throw-literal": "error",
+    "jsdoc/no-undefined-types": [
+      "error",
+      {
+        definedTypes: ["ParentNode", "void"]
+      }
+    ],
+    "jsdoc/require-returns": [
+      "error",
+      {
+        forceRequireReturn: true
+      }
+    ]
   }
 };
